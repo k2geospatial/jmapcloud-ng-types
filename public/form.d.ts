794436665ce933ca6f121d5e0f06934c6f970c9d
Nicolas Savard <nsavard@k2geospatial.com>
2025-05-23 11:16:36 -0400

    remove old code for query and form

--- public/form.d.ts
+++ public/form.d.ts
@@ -1,24 +1,11 @@
-declare interface JFormParams {
-  id: JId
-  label: string
-  schema: JFormSchema
-  uiSchema: JFormUISchema
-  validationRules?: JFormValidationRules,
-  readOnly?: boolean
-  defaultValueById?: { [id: string]: any }
-  viewId?: number
-  hideClearButton?: boolean
-  buttonLabelSubmit?: string
-  buttonLabelCancel?: string
-  buttonLabelClear?: string
-  formIsDestroyedAfterSubmit?: boolean
-  isSearch?: boolean
-  smallScreenDisplay?: boolean
-  disableSubmit?: boolean
-  submitErrors?: string[]
-  messageToDisplay?: string
-  validate: (values: any, formMetada: JFormMetaData) => { [key: string]: string }
-  onSubmit: (values: any, formMetada: JFormMetaData) => void | string | Promise<any>
-  onCancel?: () => void
-  onReset?: () => void
-}
+declare type formDialogParams = {
+  type: "layer"
+  layerId: JId
+  featureId?: JId
+  feature?: GeoJSON.Feature
+} | {
+  type: "table"
+  tableId: JId
+  row: { [key: string]: any }
+  isReadOnly: boolean
+} | null
